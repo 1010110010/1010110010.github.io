52000134cb476fdca1b98eff5bbbb21cd253f5ed
90 <hello@qq.com>
2017-07-25 17:16:15 +0800

    照片整理-更多了

---
+++ sources/js/js.js
@@ -0,0 +1,11 @@
+ function goBig(id){
+            var div = document.getElementById("div");
+            div.style.display="block";
+            var bigPhoto = "<img class='bigImg' onclick='goSmall("+id+")' src='img/img_xueshi/IMG_"+id+".JPG'>";
+            $("#div").html(bigPhoto);
+        }
+
+        function goSmall(id) {
+            var div = document.getElementById("div");
+            div.style.display = "none";
+        }
