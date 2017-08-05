40a894215c0e234ec38473646a11dea361ee8be7
90 <hello@qq.com>
2017-08-05 15:43:32 +0800

    添加导航栏

---
+++ sources/js/daohang.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wuming on 2017/8/5.
+ */
+/**
+ *
+ * 页面自动加载导航栏
+ *
+ *
+ */
+
+$(function(){
+    $("#daohang").load("../daohang.html");
+})
--- sources/js/js.js
+++ sources/js/js.js
@@ -1,7 +1,11 @@
- function goBig(id){
+/**
+ * 控制图片变大变小
+ * @param id
+ */
+        function goBig(id){
             var div = document.getElementById("div");
             div.style.display="block";
-            var bigPhoto = "<img class='bigImg' onclick='goSmall("+id+")' src='img/img_xueshi/IMG_"+id+".JPG'>";
+            var bigPhoto = "<img class='bigImg' onclick='goSmall("+id+")' src='../sources/img/img_xueshi/IMG_"+id+".JPG'>";
             $("#div").html(bigPhoto);
         }
 
